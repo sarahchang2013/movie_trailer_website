ad3ac4c654e83fc2bd779cea17ba2f8f8db18860
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-08-04 18:36:03 +0800

    finished
    Project submission

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,45 @@
 import media
 import fresh_tomatoes
 
-wind_talkers = media.Movie("Windtalkers",
-                          "Native Americans'crucial roles in the WWII",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BYWU3NmIzMmQtZjE2Mi00NDlkLWI2NDMtZWZmYjUxZWRiZDZiL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR1,0,182,268_AL_.jpg",
-                           "https://www.youtube.com/watch?v=KOmH0_F8_XI")
-matchstick_men = media.Movie("Matchstick Men",
-                             "A comedy starred by Nicolas Cage",
-                             "https://i.ytimg.com/vi/WgsmEhnIy7I/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC0IJpty4mfQ3-571CgMKmcyco4hw",
-                             "https://www.youtube.com/watch?v=WgsmEhnIy7I")
-army_of_one = media.Movie("Army of One",
-                          "A new movie starred by Nicolas Cage",
-                          "https://i.ytimg.com/vi/RYsPEl-xOv0/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLD1zrmyhvzX4Y3f9eDZxBNjqGc86Q",
-                          "https://www.youtube.com/watch?v=RYsPEl-xOv0")
-inconceivable = media.Movie("Inconceivable",
-                            "A 2017 movie starred by Nicolas Cage",
-                            "https://i.ytimg.com/vi/MaU2hf4iGew/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBL3y1BOIE0arfek3HQhefzBq_Sgw",
-                            "https://www.youtube.com/watch?v=MaU2hf4iGew")
+#Instantiate movies with titles, links to posters, and links to trailers.
+wind_talkers = media.Movie(
+                            "Windtalkers",
+                            ('https://images-na.ssl-images-amazon.com/ima'
+                             'ges/M/MV5BYWU3NmIzMmQtZjE2Mi00NDlkLWI2NDMtZW'
+                             'ZmYjUxZWRiZDZiL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzM'
+                             'zNDI@._V1_UY268_CR1,0,182,268_AL_.jpg'),
+                            "https://www.youtube.com/watch?v=KOmH0_F8_XI")
 
-movies = [wind_talkers,matchstick_men,army_of_one,inconceivable]
+matchstick_men = media.Movie(
+                              "Matchstick Men",
+                              ('https://images-na.ssl-images-amazon.com/image'
+                               's/M/MV5BMjA3NjMyNjIyMF5BMl5BanBnXkFtZTYwOTgzM'
+                               'DI3._V1_UX182_CR0,0,182,268_AL_.jpg'),
+                              "https://www.youtube.com/watch?v=WgsmEhnIy7I")
+
+army_of_one = media.Movie(
+                            "Army of One",
+                            ('https://images-na.ssl-images-amazon.com/image'
+                             's/M/MV5BZWY4NzQwNWUtYzVkYS00YjIwLWJkYmEtYmU4Mm'
+                             'E5ZjJmMjEzXkEyXkFqcGdeQXVyNTU2NDMyOTM@._V1_UY26'
+                             '8_CR4,0,182,268_AL_.jpg'),
+                            "https://www.youtube.com/watch?v=RYsPEl-xOv0")
+
+inconceivable = media.Movie(
+                             "Inconceivable",
+                               ('https://images-na.ssl-images-amazon.com/image'
+                                's/M/MV5BNWM0ODdkNTYtNjAwNS00OTE1LTlhMWMtOGE5OT'
+                                'hjM2MzMzAwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX18'
+                                '2_CR0,0,182,268_AL_.jpg'),
+                             "https://www.youtube.com/watch?v=MaU2hf4iGew")
+
+#Construct an array to hold all instances of movies.
+movies = [
+            wind_talkers, 
+            matchstick_men,
+            army_of_one, 
+            inconceivable
+         ]
+
+#Open the result in a browser.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 class Movie():
-    """This is a class of movies with title, storyline, poster and trailer"""
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """This is a class of movies with titles, posters and trailers"""
+
+    #Initialize the object with given variables.
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+
