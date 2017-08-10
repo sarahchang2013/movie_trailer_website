4e8c8b6b438b07bbd7304bbef2ca1cd0b337cf82
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-08-10 19:01:59 +0800

    updated to meet PEP8
    updated to run error free in online PEP8 checker.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,45 +1,34 @@
 import media
 import fresh_tomatoes
 
-#Instantiate movies with titles, links to posters, and links to trailers.
+# Instantiate movies with titles, links to posters, and links to trailers.
 wind_talkers = media.Movie(
                             "Windtalkers",
-                            ('https://images-na.ssl-images-amazon.com/ima'
-                             'ges/M/MV5BYWU3NmIzMmQtZjE2Mi00NDlkLWI2NDMtZW'
-                             'ZmYjUxZWRiZDZiL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzM'
-                             'zNDI@._V1_UY268_CR1,0,182,268_AL_.jpg'),
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BYWU3NmIzMmQtZjE2Mi00NDlkLWI2NDMtZWZmYjUxZWRiZDZiL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR1,0,182,268_AL_.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=KOmH0_F8_XI")
 
 matchstick_men = media.Movie(
                               "Matchstick Men",
-                              ('https://images-na.ssl-images-amazon.com/image'
-                               's/M/MV5BMjA3NjMyNjIyMF5BMl5BanBnXkFtZTYwOTgzM'
-                               'DI3._V1_UX182_CR0,0,182,268_AL_.jpg'),
+                              "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA3NjMyNjIyMF5BMl5BanBnXkFtZTYwOTgzMDI3._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                               "https://www.youtube.com/watch?v=WgsmEhnIy7I")
 
 army_of_one = media.Movie(
                             "Army of One",
-                            ('https://images-na.ssl-images-amazon.com/image'
-                             's/M/MV5BZWY4NzQwNWUtYzVkYS00YjIwLWJkYmEtYmU4Mm'
-                             'E5ZjJmMjEzXkEyXkFqcGdeQXVyNTU2NDMyOTM@._V1_UY26'
-                             '8_CR4,0,182,268_AL_.jpg'),
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BZWY4NzQwNWUtYzVkYS00YjIwLWJkYmEtYmU4MmE5ZjJmMjEzXkEyXkFqcGdeQXVyNTU2NDMyOTM@._V1_UY268_CR4,0,182,268_AL_.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=RYsPEl-xOv0")
 
 inconceivable = media.Movie(
                              "Inconceivable",
-                               ('https://images-na.ssl-images-amazon.com/image'
-                                's/M/MV5BNWM0ODdkNTYtNjAwNS00OTE1LTlhMWMtOGE5OT'
-                                'hjM2MzMzAwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX18'
-                                '2_CR0,0,182,268_AL_.jpg'),
+                             "https://images-na.ssl-images-amazon.com/images/M/MV5BNWM0ODdkNTYtNjAwNS00OTE1LTlhMWMtOGE5OThjM2MzMzAwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                              "https://www.youtube.com/watch?v=MaU2hf4iGew")
 
-#Construct an array to hold all instances of movies.
+# Construct an array to hold all instances of movies.
 movies = [
-            wind_talkers, 
+            wind_talkers,
             matchstick_men,
-            army_of_one, 
+            army_of_one,
             inconceivable
          ]
 
-#Open the result in a browser.
+# Open the result in a browser.
 fresh_tomatoes.open_movies_page(movies)
